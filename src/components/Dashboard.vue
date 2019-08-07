c45407552ca30381c09684eb6b75410dda425038
kml1326 <kmlraj131996@gmail.com>
2019-08-07 17:40:50 +0530

    initial commit

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    <Header/>
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Header from './Header'
+export default {
+  name: "Dashboard",
+  components: {
+    Header
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/router.js
@@ -0,0 +1,26 @@
+import Vue from "vue";
+import Router from "vue-router";
+import Timeline from "./views/Timeline.vue";
+
+Vue.use(Router);
+
+export default new Router({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: "/timeline",
+      name: "timeline",
+      component: Timeline
+    },
+    {
+      path: "/toolbox",
+      name: "toolbox",
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () =>
+        import(/* webpackChunkName: "about" */ "./views/Toolbox.vue")
+    }
+  ]
+});
